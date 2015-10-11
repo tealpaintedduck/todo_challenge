a9091eb9f39ad866efc299712e006a4b10aae4b0
Sarah Middleton <middleton.sarahlouise@gmail.com>
2015-10-11 15:40:16 +0100

    adds comma in list

--- e2e/conf.js
+++ e2e/conf.js
@@ -1,6 +1,6 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['toDoListFeature.js']
+  specs: ['toDoListFeature.js'],
   capabilities: {
     'browserName': 'firefox'
   }
