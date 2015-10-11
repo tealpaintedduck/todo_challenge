683f7d833ace8b06c4d1e17e4b9c6cfd5f14c6cd
Sarah Middleton <middleton.sarahlouise@gmail.com>
2015-10-11 18:52:02 +0100

    adds codeclimate yaml

--- e2e/toDoListFeature.js
+++ e2e/toDoListFeature.js
@@ -8,7 +8,7 @@ describe('To Do List', function() {
     element(by.className('btn-primary')).click();
 
     expect(element(by.binding('task.task')).getText()).toEqual("destroy omnicorp");
-  })
+  });
 
   it('removes tasks from default list when completed', function() {
     browser.get('http://localhost:8080');
@@ -16,7 +16,7 @@ describe('To Do List', function() {
     element(by.className('btn-primary')).click();
     element(by.className('btn-check')).click();
     expect(element(by.className('list-group')).getText()).toEqual("");
-  })
+  });
 
   it('shows completed tasks when selecting relevant filter', function() {
     browser.get('http://localhost:8080');
@@ -25,7 +25,7 @@ describe('To Do List', function() {
     element(by.className('btn-check')).click();
     element(by.className('filter-complete')).click();
     expect(element(by.binding('task.task')).getText()).toEqual("destroy omnicorp");
-  })
+  });
 
   it('does not show active tasks in completed filter', function() {
     browser.get('http://localhost:8080');
@@ -33,7 +33,7 @@ describe('To Do List', function() {
     element(by.className('btn-primary')).click();
     element(by.className('filter-complete')).click();
     expect(element(by.className('list-group')).getText()).toEqual("");
-  })
+  });
 
   it('shows both active and completed tasks in "all" filter', function() {
     browser.get('http://localhost:8080');
@@ -44,24 +44,24 @@ describe('To Do List', function() {
     element(by.className('btn-primary')).click();
     element(by.className('filter-all')).click();
     var tasks = element.all(by.repeater('task in listCtrl.taskList.tasks'));
-    expect(tasks.get(0).getText()).toEqual('grow limbs ✓')
-    expect(tasks.get(1).getText()).toEqual('destroy omnicorp ✓')
-  })
+    expect(tasks.get(0).getText()).toEqual('grow limbs ✓');
+    expect(tasks.get(1).getText()).toEqual('destroy omnicorp ✓');
+  });
 
   it('shows the count for active tasks', function() {
     browser.get('http://localhost:8080');
     element(by.model('listCtrl.newTask')).sendKeys("destroy omnicorp");
     element(by.className('btn-primary')).click();
-    expect(element(by.className('to-do-count')).getText()).toEqual('Tasks to be done: 1')
+    expect(element(by.className('to-do-count')).getText()).toEqual('Tasks to be done: 1');
   });
 
   it('shows the active filter', function() {
     browser.get('http://localhost:8080');
     element(by.className('filter-all')).click();
-    expect(element(by.className('current-filter')).getText()).toEqual('Displaying: All')
+    expect(element(by.className('current-filter')).getText()).toEqual('Displaying: All');
     element(by.className('filter-complete')).click();
-    expect(element(by.className('current-filter')).getText()).toEqual('Displaying: Completed')
+    expect(element(by.className('current-filter')).getText()).toEqual('Displaying: Completed');
     element(by.className('filter-active')).click();
-    expect(element(by.className('current-filter')).getText()).toEqual('Displaying: Active')
-  })
-})
+    expect(element(by.className('current-filter')).getText()).toEqual('Displaying: Active');
+  });
+});
